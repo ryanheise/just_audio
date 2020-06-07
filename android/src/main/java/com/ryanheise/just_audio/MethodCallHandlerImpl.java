637a1727f78fc53a4fd537dc2b48f481010c63db
Sebastian Roth <sebastian.roth@gmail.com>
2020-06-07 17:28:53 +0100

    Move the dispose method to the main channel

--- android/src/main/java/com/ryanheise/just_audio/MethodCallHandlerImpl.java
+++ android/src/main/java/com/ryanheise/just_audio/MethodCallHandlerImpl.java
@@ -1,20 +1,19 @@
 package com.ryanheise.just_audio;
 
 import android.content.Context;
+import android.util.SparseArray;
 import androidx.annotation.NonNull;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MethodCallHandlerImpl implements MethodCallHandler {
 
 	private final Context applicationContext;
 	private final BinaryMessenger messenger;
 
-	private final List<AudioPlayer> players = new ArrayList<>();
+	private final SparseArray<AudioPlayer> players = new SparseArray<>();
 
 	public MethodCallHandlerImpl(Context applicationContext,
 			BinaryMessenger messenger) {
@@ -25,12 +24,21 @@ public class MethodCallHandlerImpl implements MethodCallHandler {
 	@Override
 	public void onMethodCall(MethodCall call, @NonNull Result result) {
 		switch (call.method) {
-			case "init":
-				final List<?> args = (List<?>) call.arguments;
-				String id = (String) args.get(0);
-				players.add(new AudioPlayer(applicationContext, messenger, id));
+			case "init": {
+				String id = call.argument("id");
+				players.put(Integer.parseInt(id), new AudioPlayer(applicationContext, messenger, id));
 				result.success(null);
 				break;
+			}
+			case "dispose": {
+				final Integer id = call.argument("id");
+				final AudioPlayer player = players.get(id);
+
+				if (player != null) {
+					player.dispose();
+					players.remove(id);
+				}
+			}
 			case "setIosCategory":
 				result.success(null);
 				break;
@@ -41,7 +49,9 @@ public class MethodCallHandlerImpl implements MethodCallHandler {
 	}
 
 	void dispose() {
-		for (AudioPlayer player : players) {
+		final int size = players.size();
+		for (int i = 0; i < size; i++) {
+			final AudioPlayer player = players.valueAt(i);
 			player.dispose();
 		}
 
