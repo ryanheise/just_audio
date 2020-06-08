8f146e559aca25dfe1066cd5d0b10e6699482f4c
Sebastian Roth <sebastian.roth@gmail.com>
2020-06-08 12:20:14 +0100

    Android: Support Android Embedding V2 (#109)
    * Bump AGP & Gradle
    * Migrate the project to Android Embedding to V2

---
+++ android/src/main/java/com/ryanheise/just_audio/MethodCallHandlerImpl.java
@@ -0,0 +1,53 @@
+package com.ryanheise.just_audio;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MethodCallHandlerImpl implements MethodCallHandler {
+
+	private final Context applicationContext;
+	private final BinaryMessenger messenger;
+
+	private final Map<String, AudioPlayer> players = new HashMap<>();
+
+	public MethodCallHandlerImpl(Context applicationContext,
+			BinaryMessenger messenger) {
+		this.applicationContext = applicationContext;
+		this.messenger = messenger;
+	}
+
+	@Override
+	public void onMethodCall(MethodCall call, @NonNull Result result) {
+		switch (call.method) {
+		case "init":
+			final List<String> ids = call.arguments();
+			String id = ids.get(0);
+			players.put(id, new AudioPlayer(applicationContext, messenger, id,
+					() -> players.remove(id)
+			));
+			result.success(null);
+			break;
+		case "setIosCategory":
+			result.success(null);
+			break;
+		default:
+			result.notImplemented();
+			break;
+		}
+	}
+
+	void dispose() {
+		for (AudioPlayer player : players.values()) {
+			player.dispose();
+		}
+
+		players.clear();
+	}
+}
