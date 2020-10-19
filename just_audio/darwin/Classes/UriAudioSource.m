ef3a081b0d6e7a127882d5eb02a6870071a3d670
hsangtini <h.sang@tini.vn>
2020-10-19 19:18:28 +0700

    Spaces in local file paths don't work on iOS (#214)
    https://github.com/ryanheise/just_audio/issues/208
    
    Co-authored-by: Hoang Sang <buontaiviem@gmail.com>

--- just_audio/darwin/Classes/UriAudioSource.m
+++ just_audio/darwin/Classes/UriAudioSource.m
@@ -14,7 +14,7 @@
     NSAssert(self, @"super init cannot be nil");
     _uri = uri;
     if ([_uri hasPrefix:@"file://"]) {
-        _playerItem = [[IndexedPlayerItem alloc] initWithURL:[NSURL fileURLWithPath:[_uri substringFromIndex:7]]];
+        _playerItem = [[IndexedPlayerItem alloc] initWithURL:[NSURL fileURLWithPath:[[_uri stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding] substringFromIndex:7]]];
     } else {
         _playerItem = [[IndexedPlayerItem alloc] initWithURL:[NSURL URLWithString:_uri]];
     }
