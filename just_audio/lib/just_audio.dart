0d13d43cc73e00b48d9d7438d017146336b925c6
嵇珂 <ke.ji@brainco.cn>
2021-08-02 09:29:25 +0800

    fix clamp

--- just_audio/lib/just_audio.dart
+++ just_audio/lib/just_audio.dart
@@ -2707,8 +2707,8 @@ class LockCachingAudioSource extends StreamAudioSource {
       for (var cacheResponse in inProgressResponses) {
         if (_progress >= cacheResponse.end) {
           // We've received enough data to fulfill the byte range request.
-          var subEnd = (data.length - (_progress - cacheResponse.end))
-              .clamp(0, data.length);
+          var subEnd = (data.length - (_progress - cacheResponse.end));
+          subEnd = min(max(0, subEnd), data.length);
           cacheResponse.controller.add(data.sublist(0, subEnd));
           cacheResponse.controller.close();
         } else {
