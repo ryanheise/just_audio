b1cbd1623dc1eb33fdd5037bfc88f0f796e77fb6
renan jegouzo <renan@aestesis.org>
2025-09-02 08:17:51 +0200

    clean

--- just_audio/lib/just_audio.dart
+++ just_audio/lib/just_audio.dart
@@ -2473,11 +2473,7 @@ class _ProxyHttpServer {
     _running = true;
 
     if (Platform.isAndroid) {
-      //var chain = Platform.script.resolve('packages/just_audio/assets/cert.pem').toFilePath();
-      //var key = Platform.script.resolve('packages/just_audio/assets/key.pem').toFilePath();
-      var context = SecurityContext();
-        //..useCertificateChain(chain)
-        //..usePrivateKey(key, password: 'renan');
+      final context = SecurityContext();
       _server =
           await HttpServer.bindSecure(InternetAddress.loopbackIPv4, 0, context);
     } else {
